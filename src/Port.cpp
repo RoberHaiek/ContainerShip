dc2c68f8158110b66b89af0e3378173d62144ad8
Rober Haiek <rober.haiek@gmail.com>
2020-03-31 19:56:24 +0300

    Adding implementation of optimum with some other bug fixes / changes (still doesnt compile)

--- src/Port.cpp
+++ src/Port.cpp
@@ -7,17 +7,14 @@
 
 #include <iostream>
 #include <list>
-#include "Container.cpp"
 using namespace std;
 
 class Port {
 public:
 	string port="emptyString";	// 5 capital English letters
-	list<Container> tempContainers;
 
 	Port(string port){
 		this->port=port;
-		this->tempContainers=NULL;
 	}
 
 	string toString(){
