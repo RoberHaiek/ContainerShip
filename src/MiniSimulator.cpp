0bd75bdf89be61f2c3d4cad24c3a406e7cf0dc42
aubaida <n_b_a_09@hotmail.com>
2020-05-18 02:13:34 +0300

    IOhandler

--- src/MiniSimulator.cpp
+++ src/MiniSimulator.cpp
@@ -6,29 +6,16 @@
 // Description : Simulator in C++, Ansi-style
 //============================================================================
 #include <stdio.h>
-
 #include <iostream>
 #include <string.h>
 #include <dirent.h>
 #include <fstream>
 #include "Stowage.cpp"
 #include "WeightBalanceCalculator.cpp"
-#define SUCCESS 1
-#define ERROR 0
+#include "IOHandler.cpp"//may changed
 #define LAST 1
 #define REGULAR 0
-#define MAX_LINE 1024
-using namespace std;
-int width,length,maxHeight;
-char *workPath;
-char *travelPath;
-char **route;
-int routeSize;
-const char* SHIP_PLANE="ship_plan";
-const char* ROUTE="route";
-const char* OUTPUT="/output";
 const char* EXPECTED_OUTPUT="/expected_output";
-char* parse_out=new char[MAX_LINE];
 Ship* ship;
 Stowage* curAlgo;
 ofstream fd_results;
@@ -36,6 +23,7 @@ const char* fake_out="/fake_out";
 string travelName;
 string AlgoName="fakeAlgorithim";
 int numInstructions=0;
+
 /*---------------FUNC DEC-------------*/
 int getNumOfLines(ifstream& fd);
 char* getCargoFileName(int portIndex);
@@ -104,56 +92,6 @@ string** ReadExpectedInstructionsFAKE(char* cargoFileName){
 	}
 	return expectedInstructions;
 }
-
-/*--------------------------PARSING METHODS--------------------------*/
-char* getElem(string s , int& seek,char delmiter=' '){
-	int index=0;
-	if(delmiter==' '){//find the first index which not whitspace
-		while(seek < (int)s.length() && s.at(seek)==delmiter){
-			seek++;
-		}
-	}
-	while(seek < (int)s.length()){
-		/*if(delmiter==','){
-				cout << s.at(seek)<<endl;
-			}*/
-		if(s.at(seek)==delmiter){
-			break;
-		}
-		parse_out[index++]=s.at(seek++);
-	}
-	parse_out[index]='\0';
-	seek++;
-	return NULL;//must return
-}
-/*-------------------------------------------------------------------------------------------------------------*/
-
-//[17]
-
-void parseResults (string algoName,string travelName,int numInst, int port){
-	static int sum;
-
-	if(!fd_results.is_open()){
-		fd_results.open("results.txt");
-		fd_results<< "********Travel name= "<<travelName<<"\n\t"<<"Algorithem name="<< algoName<<"\n\t";
-
-	}
-	else if(fd_results.is_open() && port==0){
-		fd_results<<"---------------------------------------------------------------\n";
-		fd_results<< "********Travel name= "<<travelName<<"\n\t"<<"Algorithem name="<< algoName<<"\n\t";	
-	}
-	else{
-		sum+=numInst;
-		fd_results<< "In port "<< port <<": "<< numInst<<"\n\t";
-		
-		
-	}
-	if(port==routeSize){
-		fd_results<<"Sum: "<< sum<<"\n";
-		sum=0;
-	}
-}
-
 
 /*-------------------------------------------------------------------------------------------------------------*/
 //[16]
@@ -286,263 +224,7 @@ string** ReadExpectedInstructions(char* cargoFileName){
 	return expectedInstructions;
 }
 
-//[12]
-int checkInstructionPerPort(int portIndex,string** algoInstructions){
-	char* cargoFileName = getCargoFileName(portIndex);
-	map<string,vector<string>> InstructionsMap;
-	int fillMap=insertInstructionToMap(algoInstructions,InstructionsMap);
-	if(fillMap!=SUCCESS){
-		return ERROR;
-	}
-	string** expectedInstructions = ReadExpectedInstructions(cargoFileName);
-	cout<<endl<<"done reading expected instructions" <<endl;
-	cout<<endl<<"start checking the map content" <<endl;
 
-	int isSuccess=checkMapIfAsExpected(expectedInstructions,InstructionsMap);
-	cout<<endl<<"finished checking the map content" <<endl;
-
-	return isSuccess;
-	
-	
-}
-//[11]
-Port* getPortsFromRoute(){
-	int index;
-	Port* ports=new Port[routeSize+1];
-	for(index=0;index<routeSize;index++){
-		ports[index]=Port(string(route[index]));
-	}
-	ports[index]=Port("last");
-	return ports;
-
-}
-//[10]//not finished yet
-int instructionsOut(string** instructions,char* outName){
-	//open output file to write the instruction to output dir ??????????????????????? we must open a output dir to every algo????
-	ofstream fd_info;
-	cout << "in out" <<endl;
-	numInstructions=0;
-	char* filePath=new char[strlen(travelPath)+strlen(OUTPUT)+strlen(outName)+6];
-	
-	strcpy(filePath,travelPath);
-	strcat(filePath,OUTPUT);
-	strcat(filePath,"/");
-	strcat(filePath,outName);
-	strcat(filePath,".out");
-	cout << filePath <<endl;
-	fd_info.open(filePath,ios_base::out);//open the file to out
-	if(!fd_info){
-		std::cout << "ERROR[10][1]- can't open "<< filePath<< std::endl;
-	}
-	int instIndex=0;
-	while(instructions[instIndex][0].compare("last")!=0){
-		//uid,L/R/U,row,column,height
-		numInstructions++;
-		fd_info<< instructions[instIndex][0]<<","<<instructions[instIndex][1]<<","<<instructions[instIndex][2]<<","<<instructions[instIndex][3]<<","<<instructions[instIndex][4];
-		instIndex++;
-		if(instructions[instIndex][0].compare("last")!=0){
-			fd_info<<endl;
-		}
-	}
-	delete[] filePath;
-	fd_info.close();
-	return SUCCESS;
-}
-//[9]
-char* getCargoFileName(int portIndex){
-	int cnt=1;
-	for(int i=0;i<portIndex;i++){
-		if(strcmp(route[portIndex],route[i])==0){
-			cnt++;
-		}
-	}
-	char* fileName=new char[20];
-	strcpy(fileName,route[portIndex]);
-	strcat(fileName,"_");
-	fileName[strlen(fileName)]='0'+cnt;
-	fileName[strlen(fileName)]='\0';
-	strcat(fileName,".cargo_data");
-	return fileName;
-	
-}
-//[8] 
-    Container* parseCargoFile(char* fileName){
-	ifstream fd_info;
-	int is_err=SUCCESS;//must change this.........................................
-	Container* containers;
-	int containerNum;
-	char* filePath=new char[strlen(travelPath)+strlen(fileName)+2];
-	strcpy(filePath,travelPath);
-	strcat(filePath,"/");
-	strcat(filePath,fileName);
-	fd_info.open(filePath,ios_base::in);//open the file
-	//checking the access to the file
-	if(!fd_info){
-		std::cout << "ERROR[8][1]- can't open "<< filePath<< std::endl;
-	}
-	
-	containerNum=getNumOfLines(fd_info);//get container size
-	containers=new Container[containerNum+1];
-	containers[containerNum]=Container(0,Port(""),"last");//to know we reached the last container
-	string line;
-	int containerIndex=0;
-	int seek;
-	char* containerUID=new char[MAX_LINE];
-	char* containerDstPort=new char[MAX_LINE];
-	int containerWeight;
-	while(getline(fd_info,line)){
-		if(line.at(0)=='#'){
-			continue;
-			}
-			seek=0;
-			getElem(line,seek,',');
-			strcpy(containerUID,parse_out);
-			getElem(line,seek,',');
-			containerWeight=std::stoi(parse_out); // get length
-			getElem(line,seek,',');
-			strcpy(containerDstPort,parse_out);
-			/*is_err=checkUid(containerUID);*/
-			if(is_err==ERROR){
-				std::cout << "ERROR[8][1]- wrong container uid format  "<< containerUID<< std::endl;
-			}
-			else{
-				Port dstPort(containerDstPort);
-				containers[containerIndex]=Container(containerWeight,dstPort,containerUID);
-			}
-			containerIndex++;
-	}
-	return containers;
-}
-
-//[7] called in [5]
-int checkPortName(char* name){
-	if(strlen(name)!=5){
-		return ERROR;
-	}
-	char c;
-	for(int i=0;i<5;i++){
-		c=name[i];
-		if(c>='A' && c<='Z'){
-			continue;
-		}else{
-			if(c>='a'&& c<='z'){
-				name[i]=c+'A'-'a';
-			}else{
-				return ERROR;
-			}
-		}
-	}
-	return SUCCESS;
-}
-//[6] called in [5]
-int getNumOfLines(ifstream& fd){
-	string line;
-	int numOfLines=0;
-	while(getline(fd,line)){
-		if(line.at(0)=='#'){
-			continue;
-			}
-		numOfLines++;
-	}
-	fd.clear();
-	fd.seekg(0,ios::beg);//back to the start
-	return numOfLines;
-}
-//[5] called in [3]
-void initRoute(){
-	ifstream fd_info;
-	int is_err;
-	char* filePath=new char[strlen(travelPath)+strlen(ROUTE)+2];
-	strcpy(filePath,travelPath);
-	strcat(filePath,"/");
-	strcat(filePath,ROUTE);
-	fd_info.open(filePath,ios_base::in);//open the file
-	//checking the access to the file
-	if(!fd_info){
-		std::cout << "ERROR[5][1]- can't open "<< filePath<< std::endl;
-	}
-	routeSize=getNumOfLines(fd_info);//get route size
-	route=new char*[routeSize];
-	for(int i=0;i<routeSize;i++){
-		route[i]=new char[5];
-	}
-	//get the route
-	string line;
-	int portIndex=0;
-	int seek;
-	while(getline(fd_info,line)){
-		if(line.at(0)=='#'){
-			continue;
-			}
-			seek=0;
-			getElem(line,seek);			
-			is_err=checkPortName(parse_out);
-			if(is_err==ERROR){
-				std::cout << "ERROR[5][2]- wrong port name "<< parse_out<< std::endl;
-			}
-			if(is_err!=ERROR){
-				strcpy(route[portIndex],parse_out);
-				cout<<route[portIndex]<<endl;
-			}
-			portIndex++;
-	}
-}
-//[6] called in[4]
-void getTripleElem(string line,int& seek,int& firstElem ,int& secElem ,int& thirdElem){
-	getElem(line,seek,',');
-	firstElem=std::stoi(parse_out); // get max height
-	getElem(line,seek,',');
-	secElem=std::stoi(parse_out); // get length
-	getElem(line,seek,',');
-	thirdElem=std::stoi(parse_out);// get width
-	cout << firstElem <<","<<secElem<<","<<thirdElem<<endl;
-}
-//[4] called in [3]
-void initShipPlan(){
-	ifstream fd_info;
-	char* filePath=new char[strlen(travelPath)+strlen(SHIP_PLANE)+2];
-	strcpy(filePath,travelPath);
-	strcat(filePath,"/");
-	strcat(filePath,SHIP_PLANE);
-	fd_info.open(filePath,ios_base::in);//open the file
-	//checking the access to the file
-	if(!fd_info){
-		std::cout << "ERROR[4][1]- can't open "<< filePath<< std::endl;
-	}
-	//start parsing
-	string line;
-	bool firstLine=true;
-
-	while(getline(fd_info,line)){
-		if(line.at(0)=='#'){
-			continue;
-			}
-	
-	int seek =0;
-	cout << "the parsing line : " <<line<<endl;
-	if(firstLine){
-		getTripleElem(line,seek,maxHeight,width,length);//each seprated with coma
-		/*
-		 intiate the ship
-		*/
-		cout<<"initiate the ship :"<<width<<", "<<length<<", "<<maxHeight<<endl;
-		ship=new Ship(width,length,maxHeight);
-		cout << "ship done init"<<endl;
-
-		firstLine=!firstLine;	
-		}else{
-		int x,y,floors;
-		
-		getTripleElem(line,seek,x,y,floors);//each seprated with coma
-		if(floors<maxHeight){// you can check for an error format
-			/*
-			update ship plan
-			*/
-			(*ship).setHeight(x,y,floors);
-			}
-		}
-	}
-}
 //[3] called in [2]
 void simulateTravel(){
 	  //intiate the ship and get the route
---
+++ src/Stowage.cpp
@@ -0,0 +1,231 @@
+#include "StowageTester.cpp"
+#include "AbstractAlgorithm.h"
+#include <cctype>
+#include <queue>
+#include <sstream>
+
+class Stowage: public AbstractAlgorithm:{
+public:
+	int instNum;
+	Ship *ship;
+	Port *route;					// array of ports
+	std::string **currentInstructions;
+	std::deque<node*> tempContainers;
+	std::deque<node*> loadBackContainers;
+	std::deque<std::string*> indexies;
+	int routeIndex;
+
+	/*
+	void printContainersZeroZero(){
+		node* temp=ship->planLinkedList[0][0].linkedList;
+		cout<< "--------------------[0][0]---------------------------------"<<endl;
+
+		while(temp!=NULL){
+			cout<< temp->container->uniqueId <<" --> ";
+			temp=temp->next;
+		}
+		cout<<" NULL "<<endl;
+
+		cout<< "--------------------[0][0] END---------------------------------"<<endl;
+	}
+
+	//printing
+	void printContainers(Container *array) {
+		Container *p = array;
+		int index = 0;
+		std::cout
+				<< "--------------------CONTAINERS---------------------------------"
+				<< endl;
+		while (p->uniqueId.compare("last") != 0) {
+			std::cout << "on index " << index << " ID: " << p->uniqueId << endl;
+
+		}
+		std::cout
+				<< "------------------------END CONTAINERS-----------------------------"
+				<< endl;
+	}*/
+
+	static bool weightBalance() {
+		return true;	// we have a magical ship
+	}
+
+	int sizeOfArray(Container *array) {
+		int c = 0;
+		while (true) {
+			if (array[c].uniqueId == "last")
+				return c;
+			c++;
+		}
+		return 0;
+	}
+
+	// ask Aubaida:
+
+	int readShipPlan(const std::string& full_path_and_file_name){
+		// read the ship plan from a file
+		return 0; // success
+	}
+
+	int readShipRoute(const std::string& full_path_and_file_name){
+		// read ship route from file
+		return 0; // success
+	}
+
+	int setWeightBalanceCalculator(WeightBalanceCalculator& calculator){
+		// what???
+		return 0;
+	}
+
+	int getInstructionsForCargo(const std::string& input_full_path_and_file_name, const std::string& output_full_path_and_file_name){
+		// read from input, write to output - PARSING
+		return 0;
+	}
+
+	void fillInstructions(std::string LUR, std::string uniqueId, std::string height, std::string row,
+			std::string column) {
+		currentInstructions[instNum] = new std::string[5];
+		currentInstructions[instNum][0] = uniqueId;
+		currentInstructions[instNum][1] = LUR;
+		currentInstructions[instNum][2] = row;
+		currentInstructions[instNum][3] = column;
+		currentInstructions[instNum][4] = height;
+		instNum = instNum + 1;
+	}
+	// the logic for unloading the containers to a port
+	void unloadingAlgo(int i) {
+		bool popAllAbove; // true if we're popping a container from a stack and we need to pop all above it
+		struct node *currentContainer;
+		std::string* indxes;
+		Crane crane = Crane(this->ship);
+		for (int row = 0; row < ship->shipWidth; row++) {
+			for (int column = 0; column < ship->shipLength; column++) {
+				if (this->ship->planLinkedList[row][column].size == 0) {
+					continue;
+				}
+				popAllAbove = false;
+				cellLinkedList list=ship->planLinkedList[row][column];
+				currentContainer = list.linkedList;
+				while(currentContainer !=NULL) {		// starting from the bottom !!!
+					if (currentContainer->container->destPort.toString() == route[i].toString()) {// does ship container belong to ship port?
+						int *dimensions = ship->planMap->find(currentContainer->container->uniqueId)->second;
+							if (CraneTester::isValidUnload(row, column,dimensions[0], dimensions[1]) == 0) {
+									node *temp = crane.unload(*(currentContainer->container),row, column,this->ship->planLinkedList[row][column].size);
+							currentContainer = temp->next;
+							tempContainers.push_front(temp);
+							indxes=new std::string[3];
+							indxes[0]=std::to_string(row+1);indxes[1]=std::to_string(column+1);indxes[2]=std::to_string(this->ship->planLinkedList[row][column].size+1);
+							indexies.push_front(indxes);
+							popAllAbove = true;
+						}
+					} else {	// ship container does NOT belong to ship port
+						if (popAllAbove) {	// but should I put it in temp?
+							int *dimensions = ship->planMap->find(currentContainer->container->uniqueId)->second;
+							if (CraneTester::isValidUnload(row, column, dimensions[0], dimensions[1]) == 0) {
+								node *temp = crane.unload(*(currentContainer->container),row,column,this->ship->planLinkedList[row][column].size);
+								currentContainer = temp->next;
+								tempContainers.push_front(temp);
+								indxes=new std::string[3];
+								indxes[0]=std::to_string(row+1);indxes[1]=std::to_string(column+1);indxes[2]=std::to_string(this->ship->planLinkedList[row][column].size+1);
+								indexies.push_front(indxes);
+							}
+						}
+					}
+					if(!popAllAbove && currentContainer!=NULL){
+					currentContainer =currentContainer->next;}
+				}
+				
+				// loading containers from temp back to ship
+				node *popedElem;
+				std::string *indx;
+				while (!tempContainers.empty()) {
+					popedElem = tempContainers.front();
+					indx=indexies.front();
+					indexies.pop_front();
+					tempContainers.pop_front();
+					std::string dstPort = popedElem->container->destPort.toString();
+					fillInstructions(UNLOAD, popedElem->container->uniqueId, indx[2], indx[0], indx[1]);
+					if (dstPort.compare(route[i].toString()) == 0) {
+						delete popedElem;
+						delete[] indx;
+					} else {
+						loadBackContainers.push_front(popedElem);
+						indexies.push_back(indx);
+					}
+				}
+				while (!loadBackContainers.empty()) {
+					popedElem = loadBackContainers.back();
+					indx=indexies.back();
+					indexies.pop_back();
+					loadBackContainers.pop_back();
+					fillInstructions(LOAD,popedElem->container->uniqueId, indx[2],indx[0], indx[1]);
+					crane.load(popedElem->container, row, column,this->ship->planLinkedList[row][column].size);
+					delete popedElem;
+					delete indx;
+
+
+				}
+
+			}
+		}
+	}
+
+// the logic for loading the containers from a port
+	void loadingAlgo(Container *PortInstructions, bool (*weightBalance)()) {
+		bool breakIt = false;		// move to the next container from the port instructions list
+		struct node currentContainer;
+		Crane crane = Crane(this->ship);
+		for (int p = 0; p < sizeOfArray(PortInstructions); p++) {	// for each container in the instructions
+			currentContainer.container = &(PortInstructions[p]);
+			if (!isRejected(currentContainer)) {	// if its not rejected
+				for (int row = 0; row < ship->shipWidth; row++) {	// for each row
+					for (int column = 0; column < ship->shipLength; column++) {	// for each column
+						if (ship->planLinkedList[row][column].size <= ship->planLinkedList[row][column].maxHeight && weightBalance()) {		// check if we are below height limit and balanced
+							if (CraneTester::isValidLoad(row, column, this->ship->planLinkedList[row][column].size, ship->shipWidth, ship->shipLength, this->ship->planLinkedList[row][column].maxHeight, ship->planMap,currentContainer.container->uniqueId) == 0) {
+								crane.load(currentContainer.container, row,column,this->ship->planLinkedList[row][column].size);	// load it
+								fillInstructions(LOAD, currentContainer.container->uniqueId, std::to_string((this->ship->planLinkedList[row][column].size)), std::to_string(row+1), std::to_string(column+1));	// edit instructions
+								breakIt = true;
+								break;
+							}
+						}
+					}
+					if (breakIt) {
+						breakIt = false;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+// rejection test
+	bool isRejected(node currentContainer) {
+		if (StowageTester::isInRoute(currentContainer.container->destPort.toString(), this->route,routeIndex) != 0	// is the container's destination NOT port in route?
+				|| CraneTester::isFull(this->ship) != 0																// is the ship full?
+					|| CraneTester::isValidId(currentContainer.container->uniqueId) != 0							// is the container's unique ID invalid?
+						|| CraneTester::isLegalWeight(currentContainer.container->weight) != 0	) {					// is the container's weight illegal?
+			fillInstructions(REJECT, currentContainer.container->uniqueId,"-1", "-1", "-1");
+			return true;
+		}
+		return false;
+	}
+
+	/*/
+	 //	LETS SAIL!!
+	 //
+	 //	This function takes the index of the current port in the list of routes as the parameter i (first port: i=0)
+	 //	and the ship
+	 //	returns a list of instructions as following:
+	 //	{"load/unload/reject", a container's unique id, height, row, column}
+	 /*/
+
+	Stowage(int i, Ship *ship, Port *route, Container *instructions):ship(ship) {
+		this->instNum = 0;	// The instruction number of the returned instruction
+		this->currentInstructions = new std::string*[100];
+		this->route = route;
+		routeIndex=i;
+		unloadingAlgo(i);
+		loadingAlgo(instructions, weightBalance);
+		fillInstructions("last", "last", "last", "last", "last");
+	}
+}
+;
